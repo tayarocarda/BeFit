d79439d0b13f140b37174d14e0c3a7b5576f69b6
anastasiialyach <a_lyach@mail.ru>
2023-07-30 00:57:51 +0300

    ззз

--- javascripts/sheets.js
+++ javascripts/sheets.js
@@ -1,8 +1,8 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx2GMZAhdxo4SAsQX-czQJlJ-9xxgIussdlk4HlgHGRDPvKZTUvR0tXFEqa00TNWrg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbypui9y5uNtDj7e45aDwD9kpRIY-f1JPbWmxKMs4erfIrhx4JYZDg2-oRQ0t_NdNQS3VQ/exec'
 
 const form = document.forms['contact-form']
 
-form.addEventListener('submit', e => {
+form.addEventListener('wantmep', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Thank you! your form is submitted successfully." ))
